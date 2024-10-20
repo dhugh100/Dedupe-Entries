b7474080c870ea06465f733b6897c46870cdfd5f
dhugh100 <157187893+dhugh100@users.noreply.github.com>
2024-10-20 10:20:21 -0400

    Update domake.sh
    tuned domake

--- domake.sh
+++ domake.sh
@@ -9,8 +9,6 @@ else
   /home/dhugh/Do/doddf.sh $1
 fi  
 gcc -iquote /home/dhugh/C/dd `pkg-config --cflags gtk4` -g -o main  main.c get_folders.c load_entry_data.c traverse.c get_hash.c get_results.c show_columns.c install_property.c work_selected.c see_entry_data.c view_file.c sort_columns.c filter_columns.c work_trash.c work_options.c -lcrypto `pkg-config --libs gtk4` 
-#gcc -iquote /home/dhugh/C/dd `pkg-config --cflags gtk4` -g -o out main.o see_entry_data.o view_file.o load_entry_data.o work_selected.o install_property.o load_store.o show_columns.o get_hash.o get_group.o work_options.o get_folders.o -lcrypto `pkg-config --libs gtk4`
-#gcc -iquote /home/dhugh/C/dd `pkg-config --cflags gtk4` -g main.c see_entry_data.c view_file.c simple_entry_data.c work_selected.c  install_property.c simple_load.store.c simple_show_columns.c get_hash.c simple_get_group.c work_options.c get_folders.c -lcrypto `pkg-config --libs gtk4` 
-rm /home/dhugh/C/dd/*.o
-rm /home/dhugh/C/dd/*.i
-rm /home/dhugh/C/dd/*.s
+#rm /home/dhugh/C/dd/*.o
+#rm /home/dhugh/C/dd/*.i
+#rm /home/dhugh/C/dd/*.s
