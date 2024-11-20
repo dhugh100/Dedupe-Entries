5f58936d9610cefd17f25fd5ba193b1f1944b9b4
Dave Hugh <dave.hugh@outlook.com>
2024-11-20 14:52:12 -0500

    should be one directory with pub.key and binary rpm

--- about.c
+++ about.c
@@ -2,25 +2,26 @@
 #include "about.h"
 
 // Provide standard about dialog
+// -Build script will update version and release
 
 void
-about_cb(GSimpleAction *action,
-         GVariant *parm,
-         user_data *udp)
+about_cb (GSimpleAction *action,
+		GVariant *parm,
+		user_data *udp)
 {
-        const char *author[] = { "Author", "\nDave Hugh", NULL };
+	const char *author[] = { "Author", "\nDave Hugh", NULL };
 
-        GFile *logo_file = g_file_new_for_path("/home/dhugh/Documents/gimp.logo.png");
-        GdkTexture *example_logo = gdk_texture_new_from_file(logo_file, NULL);
-        g_object_unref(logo_file);
+	GFile *logo_file = g_file_new_for_path ("/home/dhugh/Documents/gimp.logo.png");
+	GdkTexture *example_logo = gdk_texture_new_from_file (logo_file, NULL);
+	g_object_unref (logo_file);
 
-        gtk_show_about_dialog(NULL,
-                              "program-name", "ddup",
-                              "logo", example_logo,
-                              "title", "About ddup",
-                              "authors", author,
-                              "version", "Version 0.0.0 Release 1",
-                              "comments",
-                              "A program to find and take action on duplicate files.\nRelies on SHA256 hashes to identify duplicates.",
-                              "copyright", "Copyright 2024 David Hugh", "license-type", GTK_LICENSE_GPL_3_0, NULL);
+	gtk_show_about_dialog (NULL,
+			      "program-name", "ddup",
+			      "logo", example_logo,
+			      "title", "About ddup",
+			      "authors", author,
+			      "version", "Version 0.0.0 Release 1",
+			      "comments",
+			      "A program to find and take action on duplicate files.\nRelies on SHA256 hashes to identify duplicates.",
+			      "copyright", "Copyright 2024 David Hugh", "license-type", GTK_LICENSE_GPL_3_0, NULL);
 }
