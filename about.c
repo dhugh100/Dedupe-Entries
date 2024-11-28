2b2eb17f711324a399a1588f5f7f902bf95e7f1f
Dave Hugh <dhugh100@gmail.com>
2024-11-28 13:04:12 -0500

    Tuning build, walking through checklist

--- about.c
+++ about.c
@@ -1,6 +1,7 @@
 /*
+This file, about.c, is a part of the ddup program.
+ddup is a GTK program to find and take action on duplicate files.
 
-ddup - a GTK program to find and take action on duplicate files
 Copyright (C) 2024  David Hugh
 
 This program is free software: you can redistribute it and/or modify
@@ -15,25 +16,24 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https:www.gnu.org/licenses/>.
-
 */
 
 #include "main.h"
 #include "logo.h"
 #include "about.h"
 
-/*
-Provide standard about dialog
-- Build script will update version and release
-- Build script compiles logo and header source, linked into the program, proving inline logo
-*/
+// Provide standard about dialog
+//- Build script will update version and release
+//- Build script compiles logo and header source, linked into the program, proving inline logo
 
 void
-about_cb (GSimpleAction *action, GVariant *parm, user_data *udp)
+about_cb (GSimpleAction *action, 
+	  GVariant      *parm, 
+	  user_data     *udp)
 {
-	const char *author[] = { "Author", "\nDave Hugh", NULL };
+	const char *author[] = {"Author", "\nDave Hugh", NULL};
 
-/* Get logo from inline resource compiled into ddup binary */
+	// Get logo from inline resource compiled into ddup binary
 	GResource *logo_r = logo_get_resource ();
 	GdkTexture *logo_t = gdk_texture_new_from_resource ("/ddup/ddup.png");
 
@@ -42,7 +42,7 @@ about_cb (GSimpleAction *action, GVariant *parm, user_data *udp)
 			      "title", "About ddup",
 			      "logo", logo_t,
 			      "authors", author,
-			      "version", "Version 0.1.0 Release 1",
+			      "version", "Version 0.0.0 Release 1",
 			      "comments",
 			      "A program to find and take action on duplicate files.\nRelies on SHA256 hashes to identify duplicates.",
 			      "copyright", "Copyright 2024 David Hugh", "license-type", GTK_LICENSE_GPL_3_0, NULL);
--- filter_columns.h
+++ filter_columns.h
@@ -1,8 +1,13 @@
 #ifndef filter_columns_h
 #define filter_columns_h
 
-void setup_filters(user_data *udp);
-gboolean filter_match(DupItem *, user_data *);
-void get_filters_cb(GtkWidget *, user_data *);
+void
+setup_filters (user_data * udp);
+
+gboolean
+filter_match (DupItem *, user_data *);
+
+void
+get_filters_cb (GtkWidget *, user_data *);
 
 #endif
