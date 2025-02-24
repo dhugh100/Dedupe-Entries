81635229d55f1db58105d8868e367e699ede4e79
Dave Hugh <dhugh100@gmail.com>
2025-02-24 14:19:25 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,48 @@
+# Dedupe Entries
+
+## Description
+Dedup uses SHA256 hashes to identify and take action on duplicate files. 
+
+## Installation
+
+### Compile and link
+- Setup the GTK 4 development environment.
+- Make sure openssl v3 or greater is installed.
+- Download the the c source, logo.xml, and header files.
+- Compile in-line logo.xml to a C string.
+  >  glib-compile-resources --generate-header logo.xml  
+  >  glib-compile-resources --generate-source logo.xml
+
+- Compile C programs and link.
+  >  ``gcc `pkg-config --cflags gtk4` -o dedupee lib.c  main.c get_folders.c load_entry_data.c traverse.c get_hash.c get_results.c show_columns.c install_property.c work_selected.c see_entry_data.c view_file.c sort_columns.c filter_columns.c work_trash.c work_options.c -lcrypto `pkg-config --libs gtk4` ``
+
+## Usage
+### Simple Flow Example
+- Start the application with the icon or from the command line.
+- Click the get button to select a directory. You can select multiple directories.
+- Dedup will traverse the directory(s) selected, hashing the regular files it finds, and then present the results in the left column. The right column has the name. Possible result values:
+  - Empty: The file is empty.
+  - Error [...]: There was an error accessing the directory entry.
+  - 7 Digit Number: The group number - all files with the same group number are duplicates, e.g., have the same SHA256 hash.
+  - Unique: No file in the selected folder(s) shares the same hash.
+- Select the presented entries for an action with a CTRL + Left click or Shift + Left click on the row. Multiple entries can be selected.
+- Right-click to bring up the action screen. The action screen choices vary depending on whether just one or more than one entries were selected. The actions are:
+  - Trash: Move the file to the trash. Available if single or multiple entries selected. 
+  - View: View the file in ASCII Dump format. Available if a single file is selected.
+  - Launch: Pass a selected file name to an application. Available if a single entry is selected.
+  - Copy to Clipboard: Copies the name of the entries to the clipboard. Available if a a single or multiple entries are selected.
+
+### Menu options
+- Main Menu
+  - Get.  Select the folders to search for duplicates.
+  - Sort.  Select the primary and secondary sort order columns in ascending or decending order.  Apply.
+  - Filter.  Enter a string to filter columns by result or name.  The 'not' option excludes.  The filters can be combined or not (and / or). Empty filters are always matches. Apply or clear.
+  - Search.  Highlight a row that contains the search string.  Proceed to the next row by selecting next at the prompt, or take action with a right clock.   
+  - Hamburger.  
+    - Options.  Chose which result types to include.  The choices are saved in ~/.config/.ddup.cfg
+    - About.  Program information.
+
+## License
+This project is licensed under the GPL 3.0  license - see the LICENSE file for details.
+
+# Dedupe Entries
---
+++ main.h
@@ -0,0 +1,186 @@
+// This file, main.h, is a part of the Dedupe Entries program.
+// 
+// Copyright (C) 2024  David Hugh
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https:www.gnu.org/licenses/>.
+
+#ifndef main_h
+#define main_h
+
+#include <gtk/gtk.h>
+#include <sys/stat.h>
+#include <stdint.h>
+#include <limits.h>
+#include <openssl/evp.h> // For hash calculation
+#include <assert.h> // for debugging
+#include <ctype.h> 
+#include <libgen.h> 
+
+// General
+#define READ_BUFF 16384 // Arbitrary
+#define OPTION_SIZE 4 // Byte count for gvariant
+#define SHA256_DIGEST_LENGTH 32 // SHA256 hash length
+#define PRINTABLE_CHAR_SIZE 16 // Number of bytes to format for a line
+
+// Limits
+#define MAX_FOLDERS 20 // Arbitrary
+#define MAX_ENTRIES 9999999 // Arbitrary 
+#define MAX_GROUPS (MAX_ENTRIES / 2) // Arbitrary
+
+// Char buffer sizes for strings
+#define STR_PATH PATH_MAX // From limits.h includes null
+#define STR_BNAME (NAME_MAX + 1) // From limits.h add null
+#define STR_HASH (SHA256_DIGEST_LENGTH * 2) + 1 // Two asci hex digits for hash byte + Null
+#define STR_CLIP (STR_PATH * 1000) // Clipboard mostly for small groups
+
+// Avoid typos by using
+#define STR_DIR "Directory\0"
+#define STR_EMP "Empty\0"
+#define STR_ERR "Error\0"
+#define STR_UNI "Unique\0"
+#define STR_SPACE " \0"
+
+// Define the DupItem GObject type
+#define DUP_TYPE_ITEM (dup_item_get_type ())
+G_DECLARE_FINAL_TYPE (DupItem, dup_item, DUP, ITEM, GObject)
+
+// Key type
+
+struct _DupItem {
+        GObject parent_instance;
+        const char *result;
+        const char *name;
+        const char *hash;
+        const char *file_size;
+        const char *modified;
+};
+
+// Use when searching columns
+
+typedef struct search_entry {
+        GtkWidget *res_e;
+        GtkEntryBuffer *res_eb;
+        char res_ebt[256];
+
+        GtkWidget *name_e;
+        GtkEntryBuffer *name_eb;
+        char name_ebt[256];
+
+        GtkWidget *clear_btn;
+
+        gboolean and;
+        gboolean or; 
+} search_entry;
+
+// Use when filtering columns
+
+typedef struct filter_entry {
+        GtkWidget *res_e;
+        GtkEntryBuffer *res_eb;
+        char res_ebt[256];
+
+        GtkWidget *name_e;
+        GtkEntryBuffer *name_eb;
+        char name_ebt[256];
+
+        GtkWidget *clear_btn;
+
+        gboolean and;
+        gboolean or; 
+        gboolean res_n;
+        gboolean name_n;	
+} filter_entry;
+
+// The key structure to pass between API calls
+typedef struct user_data {
+
+	// Application
+	GtkApplication *app;
+
+        // UI pointers
+        GtkWidget *main_window;
+	GtkWidget *progress_bar;
+	GtkWidget *action_window;
+	GtkWidget *trash_window;
+	GtkWidget *option_window;
+	GtkWidget *sort_window;
+	GtkWidget *column_view;
+
+	// Entry data
+	GListStore *list_store;
+	GListStore *filtered_list_store;
+	GListStore *org_list_store;
+
+	// Buttons - need to adjust sensivity
+	GtkWidget *sort_button;
+	GtkWidget *filter_button;
+	GtkWidget *search_entry;
+	GtkWidget *search_bar;
+	GtkWidget *cancel_button;
+
+	// Folder 
+	char **fdpp; // A pointer to an array of pointers to folder names
+
+	// Selection processing fields
+	DupItem *sel_item;
+	uint32_t sel_item_position;
+        GtkMultiSelection *selection;
+	GtkBitset *sel_bitset;
+
+	// Filter 
+	uint32_t remove_position;
+        GtkWidget *filter_window;
+        GtkFilterListModel  *filter;
+        GtkCustomFilter *custom_filter;
+	filter_entry *fep;
+
+	// Search
+	GtkWidget *search_window;
+	search_entry *sep;
+	GtkWidget *entry;
+	uint32_t next_check;
+	gboolean a_match;
+
+	// Sort
+	GtkWidget *result_a_button;
+        GtkWidget *result_d_button;
+        GtkWidget *name_a_button;
+        GtkWidget *name_d_button;
+        GtkWidget *name_a_sec_btn;
+        GtkWidget *name_d_sec_btn;
+
+	// View file
+	GtkStringList *str_list; 
+
+	// Clipboard
+	GdkClipboard *clippy;
+
+	// Switches
+        gboolean cancel_request; // True if user has requested a cancel
+        gboolean cancel_string_load; // True when view file windows is closed
+        gboolean ut_active; // True when view file windows is closed
+
+	// Options
+	char *opt_name;
+	gboolean opt_changed;
+	gboolean opt_include_empty;
+        gboolean opt_include_directory;
+        gboolean opt_include_duplicate;
+        gboolean opt_include_unique;
+
+} user_data;
+
+void option_init(user_data *udp);
+
+#endif
